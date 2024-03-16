5e822978c3ee53dd08f6e04e26cc203c7f33a926
Lakshya Bhutani <110778704+tani118@users.noreply.github.com>
2024-03-16 09:45:10 +0530

    added body for the message

--- automated_mail.py
+++ automated_mail.py
@@ -1,23 +1,24 @@
 import smtplib
+import random
 import os
 from email.message import EmailMessage
 import ssl
+otp = random.randint(10000, 99999)
 
 
 email_sender= 'studies276@gmail.com'
 email_password='rjtwaznscdcghxol'
 email_receiver=input("enter the receiver's email id=>>")
 
-subject='test for mail sending through python'
-body="""
-gaya kya??
-"""
+subject='Verification from ProGro'
+body = f"Dear User,\n\nYour verification code is: {otp}\n\nPlease use this code to verify your email address.\n\nThank you,\nYourAppName Team"
+
 
 em= EmailMessage()
 em['From']=email_sender
 em['To']=email_receiver
 em['Subject']=subject
-em.set_content(body)
+em.set_content(body) 
 
 context=ssl.create_default_context()
 
